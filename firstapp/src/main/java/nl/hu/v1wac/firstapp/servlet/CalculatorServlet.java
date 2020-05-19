f1b2f4a4a92b1ee17626e29599b87f79e31a88de
Jesse <jessepols@gmail.com>
2020-05-19 12:42:10 +0200

    persistency to postgress worlddb

--- firstapp/src/main/java/nl/hu/v1wac/firstapp/servlet/CalculatorServlet.java
+++ firstapp/src/main/java/nl/hu/v1wac/firstapp/servlet/CalculatorServlet.java
@@ -1,18 +1,96 @@
 package nl.hu.v1wac.firstapp.servlet;
 
+import nl.hu.v1wac.firstapp.model.Country;
+import nl.hu.v1wac.firstapp.persistence.CountryPostgresDaoImpl;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 @WebServlet(urlPatterns = "/CalculatorServlet.do")
 public class CalculatorServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
+		CountryPostgresDaoImpl countryDao = null;
+		try {
+			countryDao = new CountryPostgresDaoImpl();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Getting country by code...");
+			Country country = countryDao.findByCode("AD");
+			System.out.println(country);
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Getting all countries...");
+			List<Country> countries = countryDao.findAll();
+			System.out.println(countries);
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Saving a country...");
+			Country country = new Country("AA", "AAA", "AAAAAAAAAh", "FunkyTown", "Europe", "Afrika", 0.0, 0, "Dictatorship", 0.0, 0.0);
+			System.out.println(countryDao.save(country));
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Updating a country...");
+			Country country = new Country("AA", "BBB", "AAAAAAAAAh", "FunkyTown", "Europe", "Afrika", 0.0, 0, "Dictatorship", 0.0, 0.0);
+			System.out.println(countryDao.update(country));
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Deleting a country...");
+			Country country = new Country("AA", "AAA", "AAAAAAAAAh", "FunkyTown", "Europe", "Afrika", 0.0, 0, "Dictatorship", 0.0, 0.0);
+			System.out.println(countryDao.delete(country));
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Getting the 10 largest populations...");
+			List<Country> countries = countryDao.find10LargestPopulations();
+			System.out.println(countries);
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Getting the 10 largest surfaceareas...");
+			List<Country> countries = countryDao.find10LargestSurfaces();
+			System.out.println(countries);
+			System.out.println("Success!\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		Double firstNumber = 0.0;
 		Double secondNumber = 0.0;
 		String chosenOperator = "add";
@@ -50,7 +128,7 @@ public class CalculatorServlet extends HttpServlet {
 		out.println(" <body>");
 		out.println("  <h2>Result:</h2>");
 		out.println("  <h2>" + result + "</h2>");
-		out.println("  <a href='http://localhost:8081/firstapp' class='btn btn-default btn-custom'>Terug</a>");
+		out.println("  <a href='http://localhost:8080/firstapp' class='btn btn-default btn-custom'>Terug</a>");
 		out.println(" </body>");
 		out.println("</html>");		
 	}
