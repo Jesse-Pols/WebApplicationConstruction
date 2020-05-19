719cb2dd9d7de3dcfb97a3e19ff33e44e90ba600
Jesse <jessepols@gmail.com>
2020-05-19 12:50:24 +0200

    integrated perstency into worldservice

--- firstapp/src/main/java/nl/hu/v1wac/firstapp/model/ServiceProvider.java
+++ firstapp/src/main/java/nl/hu/v1wac/firstapp/model/ServiceProvider.java
@@ -1,7 +1,15 @@
 package nl.hu.v1wac.firstapp.model;
 
 public class ServiceProvider {
-	private static WorldService worldService = new WorldService();
+	private static WorldService worldService;
+
+	static {
+		try {
+			worldService = new WorldService();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static WorldService getWorldService() {
 		return worldService;
